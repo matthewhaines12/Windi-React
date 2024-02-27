f0c12c537c6792f7753149c5ddb8b004dfb77db7
Owen-Miele <157318408+Owen-Miele@users.noreply.github.com>
2024-02-26 19:53:43 -0500

    NavBar With Routing
    navbar with
    -logo(can be used as home button)
    -Radar, Home, hourly buttons
    -non-functional search bar
    -can be viewed on mobile/smaller screen
    download in VS terminal
    npm install react-router-dom
    npm install react-icons

---
+++ src/routes/Radar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GiRadarSweep } from "react-icons/gi";
+
+function Radar() {
+  return (
+    <div className="radar">
+      <h1>Radar</h1>
+      <GiRadarSweep className="page-icon" />
+    </div>
+  );
+}
+
+export default Radar;
