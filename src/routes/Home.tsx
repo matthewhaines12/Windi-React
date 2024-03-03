c95489e242305ac404478ee3167575f418cbec69
matthewhaines12 <hainesmatthew12@gmail.com>
2024-03-03 03:12:19 -0500

    messing around

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { AiOutlineHome } from "react-icons/ai";
+import { createRoot } from "react-dom/client";
+import {createBrowserRouter, RouterProvider,Route,Link,Outlet,} from "react-router-dom";
+import "./App.css";
+import "./Home.css"
+
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
-      <AiOutlineHome className="page-icon" />
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Altoona</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>55°F</p>
+          </div>
+          <div className="uv">
+            <p>UV Index 4</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
