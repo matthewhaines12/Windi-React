4ab8cce81448748c951f8d91f8b707a7a2536a7e
matthewhaines12 <hainesmatthew12@gmail.com>
2024-03-04 02:58:17 -0500

    Not sure what I am doing here

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState , useEffect} from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import CurrentWeather from "../../src/Components/CurrentWeather"
@@ -108,11 +109,16 @@ Matthew's original code:
 import React, { useState } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 // import CurrentWeather from "./Components/CurrentWeather.tsx"  // why error here??
+=======
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {createBrowserRouter, RouterProvider,Route,Link,Outlet,} from "react-router-dom";
+import "./App.css";
+import "./Home.css"
+
+>>>>>>> parent of 566aeec (home screen attempt)
 
 function Home() {
-  // const currWeather = CurrentWeather(); // definitley wont work wont work
-
-  // filler info for now
   return (
     <div className="home">
       <div className="container">
@@ -121,64 +127,23 @@ function Home() {
             <p>Altoona</p>
           </div>
           <div className="temp">
+<<<<<<< HEAD
 //            { <h1>{currWeather}°F</h1> This definitely wont work }
             <h1>63°F</h1>
+=======
+            <h1>60°F</h1>
+>>>>>>> parent of 566aeec (home screen attempt)
           </div>
           <div className="description">
             <p>Cloudy</p>
           </div>
         </div>
-        <div className="middle">
-          <table>
-            <caption>5-Day Forecast</caption>
-            <thead>
-              <tr>
-                <th>Day</th>
-                <th>Temperature</th>
-                <th>Weather</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Monday</td>
-                <td>72°F</td>
-                <td>Sunny</td>
-              </tr>
-              <tr>
-                <td>Tuesday</td>
-                <td>68°F</td>
-                <td>Partly Cloudy</td>
-              </tr>
-              <tr>
-                <td>Wednesday</td>
-                <td>70°F</td>
-                <td>Rainy</td>
-              </tr>
-              <tr>
-                <td>Thursday</td>
-                <td>75°F</td>
-                <td>Cloudy</td>
-              </tr>
-              <tr>
-                <td>Friday</td>
-                <td>78°F</td>
-                <td>Sunny</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
         <div className="bottom">
           <div className="feels">
             <p>55°F</p>
-            <p>Feels Like</p>
           </div>
           <div className="uv">
-            <p>4</p>
-            <p>UV Index</p>
-          </div>
-          <div className="wind">
-            <p>5mph</p>
-            <p>Wind Speed</p>
+            <p>UV Index 4</p>
           </div>
         </div>
       </div>
