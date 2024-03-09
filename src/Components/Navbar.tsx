d7c6572e340e7e4389d95b811f5fea98caeb8859
matthewhaines12 <hainesmatthew12@gmail.com>
2024-03-09 18:45:17 -0500

    update

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -16,69 +16,69 @@ function Navbar() {
     setClick(
       false
     ); /* Function to close the mobile menu when a link is clicked*/
-  // return (
-  // <>
-  //   <IconContext.Provider value={{ color: "#fff" }}>
-  //     <nav className="navbar">
-  //       <div className="navbar-container container">
-  //         <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-  //           <CustomIcon className="navbar-icon" />
-  //           Windi
-  //         </Link>
-  //         <div className="menu-icon" onClick={handleClick}>
-  //           {click ? <FaTimes /> : <FaBars />}
-  //         </div>
-  //         <ul className={click ? "nav-menu active" : "nav-menu"}>
-  //           <form className="search-box" role="search">
-  //             <input
-  //               className="search-input"
-  //               type="search"
-  //               placeholder="City/Zipcode"
-  //               aria-label="City/Zipcode"
-  //             />
-  //             <button className="search-button" type="submit">
-  //               Search
-  //             </button>
-  //           </form>
-  //           <li className="nav-item">
-  //             <NavLink
-  //               to="/"
-  //               className={({ isActive }) =>
-  //                 "nav-links" + (isActive ? " activated" : "")
-  //               }
-  //               onClick={closeMobileMenu}
-  //             >
-  //               Home
-  //             </NavLink>
-  //           </li>
-  //           <li className="nav-item">
-  //             <NavLink
-  //               to="/hourly"
-  //               className={({ isActive }) =>
-  //                 "nav-links" + (isActive ? " activated" : "")
-  //               }
-  //               onClick={closeMobileMenu}
-  //             >
-  //               Hourly
-  //             </NavLink>
-  //           </li>
-  //           <li className="nav-item">
-  //             <NavLink
-  //               to="/radar"
-  //               className={({ isActive }) =>
-  //                 "nav-links" + (isActive ? " activated" : "")
-  //               }
-  //               onClick={closeMobileMenu}
-  //             >
-  //               Radar
-  //             </NavLink>
-  //           </li>
-  //         </ul>
-  //       </div>
-  //     </nav>
-  //   </IconContext.Provider>
-  // </>
-  // );
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <nav className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <CustomIcon className="navbar-icon" />
+              Windi
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <form className="search-box" role="search">
+                <input
+                  className="search-input"
+                  type="search"
+                  placeholder="City/Zipcode"
+                  aria-label="City/Zipcode"
+                />
+                <button className="search-button" type="submit">
+                  Search
+                </button>
+              </form>
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    "nav-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={closeMobileMenu}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/hourly"
+                  className={({ isActive }) =>
+                    "nav-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={closeMobileMenu}
+                >
+                  Hourly
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/radar"
+                  className={({ isActive }) =>
+                    "nav-links" + (isActive ? " activated" : "")
+                  }
+                  onClick={closeMobileMenu}
+                >
+                  Radar
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
 export default Navbar;
